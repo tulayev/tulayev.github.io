94c676a20e87b7178b88b98bbdb0dd1fdaa53fec
Umarbek97 <69952016+Umarbek97@users.noreply.github.com>
2021-03-22 15:06:12 +0500

    Add files via upload

---
+++ pulse/js/script.js
@@ -0,0 +1,137 @@
+$(document).ready(function(){
+
+    // Carousel
+
+    $('.carousel__inner').slick({
+        speed: 1000,
+        //adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+
+    // Tabs
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    }
+
+    // Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn();
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut();
+    });
+
+    $('.overlay').on('click', function(e) {
+        if (e.target !== this)
+            return;
+        
+        $('.overlay, #consultation, #order, #thanks').fadeOut();
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            
+            $('.overlay, #order').fadeIn();
+        });
+    });
+
+    // Forms validation
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Пожалуйста, введите своё имя",
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свой e-mail",
+                  email: "Ваш e-mail должен быть в формате name@domain.com"
+                }
+            }
+        });
+    }
+
+    validateForms('#consultation form');
+    validateForms('#order form');
+    validateForms('#consultation-form');
+
+    // Phone number mask
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    // E-mail sending
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: 'mailer/smart.php',
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find('input').val('');
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+
+        return false;
+    });
+
+    // Smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function(){
+            const _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+    });
+
+    // WOW JS animation
+
+    new WOW().init();
+
+});
